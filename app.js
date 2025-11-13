efcedb36f440f812a2520f14591227deffefa964
gysev123 <gysev702@gmail.com>
2025-11-13 23:28:14 +0500

    app.js

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ const autoRoutes = require("./routes/auth.js");
 app.use(express.static("public"));
 app.use("/", autoRoutes);
 
+app.get('/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({ success: true, time: result.rows[0].now });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`сервер запущен на http://localhost:${PORT}`);
 });
+
