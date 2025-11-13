95709345885420e00d89a6ee2ee1c26212293da2
gysev123 <gysev702@gmail.com>
2025-11-13 23:37:18 +0500

    app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 require("dotenv").config();
-const pool = require("./bd.js");
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,17 +10,9 @@ const autoRoutes = require("./routes/auth.js");
 app.use(express.static("public"));
 app.use("/", autoRoutes);
 
-app.get('/test-db', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT NOW()');
-    res.json({ success: true, time: result.rows[0].now });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
 app.listen(PORT, () => {
   console.log(`сервер запущен на http://localhost:${PORT}`);
 });
+
 
 
